3e793aa85f6e0a0da07036ff357d8f4457dcc86a
Natalia <lesyeuxvirtuels@gmail.com>
2020-02-09 14:08:51 -0700

    created mario file

---
+++ week1/mario.c
@@ -0,0 +1,29 @@
+#include<cs50.h>
+#include <stdio.h>
+int get_positive_int(void);
+int main(void)
+
+//Prompt user for a positive integer
+
+{
+  int n;
+  do {
+n = get_int("Positive Integer:");
+  }
+  while(n<1);
+  return n;
+}
+
+//add dots
+
+
+//print hashes
+
+for(int = 0; i<8; i++)
+{
+printf("#");
+}
+printf("\n");
+}
+
+
